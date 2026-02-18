1cb321f7e50c93b6087e7b32f6a245575ec38f88
GuilleBT <guillermobeltrantabares@gmail.com>
2026-02-18 19:54:27 +0100

    creacion de componentes base

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  supabase: {
+    url: 'https://iesrwgzrzleivoyahmdt.supabase.co',
+    publicKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imllc3J3Z3pyemxlaXZveWFobWR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0MzQ0MTksImV4cCI6MjA4NzAxMDQxOX0.aOPv58WXSY0zWIvpm6bATHkWdXnxEF43nEspJAQr7hk'
+  }
+};
